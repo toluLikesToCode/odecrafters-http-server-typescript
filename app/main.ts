8f5024d56dc7cd188e04958d9455d53dcd9ccf27
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 00:31:17 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,8 @@ console.log("Hi Tolu");
 
 const server = net.createServer((socket) => {
     socket.on("clsoe", () => {
-        console.log("Client disconnected");
         socket.end();
+        console.log("Client disconnected");
     });
 });
 
