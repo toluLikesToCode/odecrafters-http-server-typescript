b83ffb27bcc107e9c48c28dab15ad4fac02c55b7
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 00:28:46 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,3 +2,10 @@ import * as net from "net";
 
 console.log("Hi Tolu");
 
+const server = net.createServer((socket) => {
+    socket.on("clsoe", () => {
+        socket.end();
+    });
+});
+
+server.listen(4221, 'localhost');
