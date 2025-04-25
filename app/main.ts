ea195e953875cc9a5cee59e96c53614b568e418b
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 01:24:03 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,27 +2,27 @@ import * as net from "net";
 
 console.log("Hi Tolu");
 
-const CLRNL = "\r\n";
-const HTTP_OK = "HTTP/1.1 200 OK" + CLRNL;
-const HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found" + CLRNL;
-const HTTP_BAD_REQUEST = "HTTP/1.1 400 Bad Request" + CLRNL;
-const CONTENT_TYPE = "Content-Type: text/html" + CLRNL;
-const CONTENT_LENGTH = "Content-Length: 13" + CLRNL;
-const HTML = "<h1>Hello</h1>" + CLRNL;
-const HTTP_RESPONSE = HTTP_OK + CONTENT_TYPE + CONTENT_LENGTH + CLRNL + HTML;
+const CLRF = "\r\n";
+const HTTP_OK = "HTTP/1.1 200 OK" + CLRF;
+const HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found" + CLRF;
+const HTTP_BAD_REQUEST = "HTTP/1.1 400 Bad Request" + CLRF;
+const CONTENT_TYPE = "Content-Type: text/html" + CLRF;
+const CONTENT_LENGTH = "Content-Length: 13" + CLRF;
+const HTML = "<h1>Hello</h1>" + CLRF;
+const HTTP_RESPONSE = HTTP_OK + CONTENT_TYPE + CONTENT_LENGTH + CLRF + HTML;
 const HTTP_RESPONSE_LENGTH = HTTP_RESPONSE.length;
 const HTTP_RESPONSE_HEADER = HTTP_OK + CONTENT_TYPE + CONTENT_LENGTH;
 const HTTP_RESPONSE_BODY = HTML;
-const HTTP_RESPONSE_END = CLRNL + CLRNL;
+const HTTP_RESPONSE_END = CLRF + CLRF;
 const HTTP_RESPONSE_FULL = HTTP_RESPONSE_HEADER + HTTP_RESPONSE_BODY + HTTP_RESPONSE_END;
 const HTTP_RESPONSE_FULL_LENGTH = HTTP_RESPONSE.length;
 const HTTP_RESPONSE_END_LENGTH = HTTP_RESPONSE_END.length;
 const HTTP_RESPONSE_HEADER_LENGTH = HTTP_RESPONSE_HEADER.length;
 const HTTP_RESPONSE_BODY_LENGTH = HTTP_RESPONSE_BODY.length;
-const HTTP_RESPONSE_HEADER_END = HTTP_RESPONSE_HEADER + CLRNL;
-const HTTP_RESPONSE_BODY_END = HTTP_RESPONSE_BODY + CLRNL;
-const HTTP_RESPONSE_END_HEADER = HTTP_RESPONSE_END + CLRNL;
-const HTTP_RESPONSE_END_BODY = HTTP_RESPONSE_END + CLRNL;
+const HTTP_RESPONSE_HEADER_END = HTTP_RESPONSE_HEADER + CLRF;
+const HTTP_RESPONSE_BODY_END = HTTP_RESPONSE_BODY + CLRF;
+const HTTP_RESPONSE_END_HEADER = HTTP_RESPONSE_END + CLRF;
+const HTTP_RESPONSE_END_BODY = HTTP_RESPONSE_END + CLRF;
 
 
 const server = net.createServer((socket) => {
@@ -32,7 +32,7 @@ const server = net.createServer((socket) => {
         // Parse the request
         const request = data.toString();
         // Split the request into lines
-        const requestLines = request.split(CLRNL);
+        const requestLines = request.split(CLRF);
         // Get the request method and path
         const requestLine = requestLines[0];
         const requestMethod = requestLine.split(" ")[0];
@@ -45,12 +45,12 @@ const server = net.createServer((socket) => {
             if (requestPath === "/abcdefg") {
                 console.log("Request path", requestPath);
                 // Send the response
-                socket.write(HTTP_NOT_FOUND);
+                socket.write(Buffer.from(HTTP_NOT_FOUND + CLRF));
                 console.log("Response sent")
             } else if (requestPath.startsWith("/")) {
                 console.log("Request path", requestPath);
                 // Send the response
-                socket.write(HTTP_OK);
+                socket.write(Buffer.from(HTTP_OK + CLRF));
                 console.log("Response sent")
             }
         }
