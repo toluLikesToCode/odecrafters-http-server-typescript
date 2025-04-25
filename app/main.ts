873df857c8f443dd2b400f3b17cac9b017d677f3
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 01:52:02 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,24 +5,12 @@ console.log("Hi Tolu");
 const CLRF = "\r\n";
 const HTTP_OK = "HTTP/1.1 200 OK" + CLRF;
 const HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found" + CLRF;
+const CONTENT_TYPE = "Content-Type: " + CLRF;
+const CONTENT_LENGTH = "Content-Length: " + CLRF;
+
+
 const HTTP_BAD_REQUEST = "HTTP/1.1 400 Bad Request" + CLRF;
-const CONTENT_TYPE = "Content-Type: text/html" + CLRF;
-const CONTENT_LENGTH = "Content-Length: 13" + CLRF;
-const HTML = "<h1>Hello</h1>" + CLRF;
-const HTTP_RESPONSE = HTTP_OK + CONTENT_TYPE + CONTENT_LENGTH + CLRF + HTML;
-const HTTP_RESPONSE_LENGTH = HTTP_RESPONSE.length;
-const HTTP_RESPONSE_HEADER = HTTP_OK + CONTENT_TYPE + CONTENT_LENGTH;
-const HTTP_RESPONSE_BODY = HTML;
-const HTTP_RESPONSE_END = CLRF + CLRF;
-const HTTP_RESPONSE_FULL = HTTP_RESPONSE_HEADER + HTTP_RESPONSE_BODY + HTTP_RESPONSE_END;
-const HTTP_RESPONSE_FULL_LENGTH = HTTP_RESPONSE.length;
-const HTTP_RESPONSE_END_LENGTH = HTTP_RESPONSE_END.length;
-const HTTP_RESPONSE_HEADER_LENGTH = HTTP_RESPONSE_HEADER.length;
-const HTTP_RESPONSE_BODY_LENGTH = HTTP_RESPONSE_BODY.length;
-const HTTP_RESPONSE_HEADER_END = HTTP_RESPONSE_HEADER + CLRF;
-const HTTP_RESPONSE_BODY_END = HTTP_RESPONSE_BODY + CLRF;
-const HTTP_RESPONSE_END_HEADER = HTTP_RESPONSE_END + CLRF;
-const HTTP_RESPONSE_END_BODY = HTTP_RESPONSE_END + CLRF;
+
 
 
 const server = net.createServer((socket) => {
@@ -39,9 +27,18 @@ const server = net.createServer((socket) => {
         if (requestMethod === "GET") {
             console.log("GET request received");
             // Check if the request path is "/"
-            const response = requestPath === '/' ? HTTP_OK + CLRF : HTTP_NOT_FOUND+ CLRF;
-            console.log("Request path", requestPath);
-            socket.write(Buffer.from(response));  
+            if (requestPath === "/echo") {
+                // Send a response
+                const responseBody = requestPath.split('/')[1];
+                const responseHeaders = [
+                    HTTP_OK,
+                    CONTENT_TYPE + "text/plain" + CLRF,
+                    CONTENT_LENGTH + responseBody.length + CLRF,
+                ].join("");
+                socket.write(Buffer.from(responseHeaders + responseBody));
+            };
+            
+             
         }
     });
 
