a981a24ba2147b9452cbea52a5eb5a327067806c
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 00:11:28 -0700

    first

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,4 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Hi Tolu");
 
-// Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
