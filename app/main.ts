de76d24db9f4503d056fa3145dc33758f8a81705
Tolu Adegbehingbe <71156414+toluLikesToCode@users.noreply.github.com>
2025-04-25 01:54:28 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -33,7 +33,7 @@ const server = net.createServer((socket) => {
                 const responseHeaders = [
                     HTTP_OK,
                     CONTENT_TYPE + "text/plain" + CLRF,
-                    CONTENT_LENGTH + responseBody.length + CLRF,
+                    CONTENT_LENGTH + responseBody.length + CLRF + CLRF,
                 ].join("");
                 socket.write(Buffer.from(responseHeaders + responseBody));
             };
